a6dd6b1df007b998779462a3545caa47e9546d35
Donald E. Willcox <dewillcox@lbl.gov>
2019-04-01 14:42:57 -0700

    Move driver from class file to a separate script in the root directory.

--- SparseGaussJordan/SparseGaussJordan.py
+++ SparseGaussJordan/SparseGaussJordan.py
@@ -545,30 +545,3 @@ class GaussJordan(object):
         fo.write('{}end subroutine gauss_jordan_solve\n'.format(indent))
         fo.write('end module gauss_jordan_module\n')
         fo.close()
-
-## Run
-if __name__=='__main__':
-    ## Read CMD Line Arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('maskfile', type=str,
-                        help='Name of the input mask file for the array A in A*x=b.')
-    parser.add_argument('-csr', action='store_true',
-                        help='Use compressed sparse row (CSR) matrix format.')
-    parser.add_argument('-py', type=str,
-                        help='Name of the Python output file to generate.')
-    parser.add_argument('-f95', type=str,
-                        help='Name of the Fortran-95 output file to generate.')
-    parser.add_argument('-cpp', type=str,
-                        help='Name of the C++ output file to generate.')
-    parser.add_argument('-smp', action='store_true',
-                        help='Attempt to simplify solution. Can be very slow, but if possible, will reduce the number of operations required for the solution.')
-    parser.add_argument('-expand', action='store_true',
-                        help='Simplify resulting expressions using Sympy expand()')
-    parser.add_argument('-cse', action='store_true',
-                        help='Execute Common Subexpression Elimination. (After simplification if the -smp option is present.) This is pretty fast.')
-    parser.add_argument('-v', action='store_true',
-                        help='Enable verbose output.')
-    args = parser.parse_args()
-    
-    if args.maskfile:
-        GJ = GaussJordan(args.maskfile, args.csr, args.py, args.f95, args.cpp, args.smp, args.expand, args.cse, args.v)
