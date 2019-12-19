148d0bd05976013ce2ef4baf85e93d35f12a37c4
Donald E. Willcox <eugene.willcox@gmail.com>
2019-12-19 14:19:22 -0800

    Fix packaging

--- SparseGaussJordan/__init__.py
+++ SparseGaussJordan/__init__.py
@@ -1,6 +1,6 @@
-from .SparseGaussJordan import GaussJordan
-from .Element import Element1D, Element2D
-from .Row import Row
-from .MatrixMath import MatrixMath
-from .RandomTesting import RandomTesting
+from SparseGaussJordan.SparseGaussJordan import GaussJordan
+from SparseGaussJordan.Element import Element1D, Element2D
+from SparseGaussJordan.Row import Row
+from SparseGaussJordan.MatrixMath import MatrixMath
+from SparseGaussJordan.RandomTesting import RandomTesting
 
