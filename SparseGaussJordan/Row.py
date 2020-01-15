0cfdd4e44301766f7ed138a892a545f527b4e9fb
Donald E. Willcox <eugene.willcox@gmail.com>
2020-01-14 17:27:59 -0800

    Revert "Remove checks for isinstance() for Row-scalar math operations for speed."
    The commit this reverts introduced bugs in generated code. Looks like we need the isinstance() checks after all.
    
    This reverts commit 535f3d4a380f7f40861aaaae2e05ab4c52dddf7f.

--- SparseGaussJordan/Row.py
+++ SparseGaussJordan/Row.py
@@ -47,10 +47,13 @@ class Row(object):
                 for es, eo in zip(self.elements, other.elements):
                     sum_elements.append(es+eo)
                 return Row(sum_elements)
+        elif isinstance(other, (int, float) + tuple(sympy.core.all_classes)):
+            sum_elements = []
+            for es in self.elements:
+                sum_elements.append(es+other)
+            return Row(sum_elements)
         else:
-            for i, es in enumerate(self.elements):
-                self.elements[i] = es + other
-            return self
+            return NotImplemented
 
     def __sub__(self, other):
         if isinstance(other, Row):
@@ -62,10 +65,13 @@ class Row(object):
                 for es, eo in zip(self.elements, other.elements):
                     sum_elements.append(es-eo)
                 return Row(sum_elements)
+        elif isinstance(other, (int, float) + tuple(sympy.core.all_classes)):
+            sum_elements = []
+            for es in self.elements:
+                sum_elements.append(es-other)
+            return Row(sum_elements)
         else:
-            for i, es in enumerate(self.elements):
-                self.elements[i] = es - other
-            return self
+            return NotImplemented
 
     def __radd__(self, other):
         return self.__add__(other)
@@ -80,10 +86,13 @@ class Row(object):
                 for es, eo in zip(self.elements, other.elements):
                     sum_elements.append(eo-es)
                 return Row(sum_elements)
+        elif isinstance(other, (int, float) + tuple(sympy.core.all_classes)):
+            sum_elements = []
+            for es in self.elements:
+                sum_elements.append(other-es)
+            return Row(sum_elements)
         else:
-            for i, es in enumerate(self.elements):
-                self.elements[i] = other - es
-            return self
+            return NotImplemented
 
     def __mul__(self, other):
         if isinstance(other, Row):
@@ -96,10 +105,13 @@ class Row(object):
                 for es, eo in zip(self.elements, other.elements):
                     mul_elements.append(eo*es)
                 return Row(mul_elements)
+        elif isinstance(other, (int, float) + tuple(sympy.core.all_classes)):
+            mul_elements = []
+            for es in self.elements:
+                mul_elements.append(es*other)
+            return Row(mul_elements)
         else:
-            for i, es in enumerate(self.elements):
-                self.elements[i] = es * other
-            return self
+            return NotImplemented
 
     def __rmul__(self, other):
         return self.__mul__(other)
@@ -115,10 +127,13 @@ class Row(object):
                 for es, eo in zip(self.elements, other.elements):
                     div_elements.append(es/eo)
                 return Row(div_elements)
+        elif isinstance(other, (int, float) + tuple(sympy.core.all_classes)):
+            div_elements = []
+            for es in self.elements:
+                div_elements.append(es/other)
+            return Row(div_elements)
         else:
-            for i, es in enumerate(self.elements):
-                self.elements[i] = es / other
-            return self
+            return NotImplemented
         
     def __rtruediv__(self, other):
         if isinstance(other, Row):
@@ -131,10 +146,13 @@ class Row(object):
                 for es, eo in zip(self.elements, other.elements):
                     div_elements.append(eo/es)
                 return Row(div_elements)
+        elif isinstance(other, (int, float) + tuple(sympy.core.all_classes)):
+            div_elements = []
+            for es in self.elements:
+                div_elements.append(other/es)
+            return Row(div_elements)
         else:
-            for i, es in enumerate(self.elements):
-                self.elements[i] = other / es
-            return self
+            return NotImplemented
 
     def fnzero(self):
         # Finds the first nonzero element in the Row: x
